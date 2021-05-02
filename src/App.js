394f44cbdc2630bab1da7dc7c79668106853860b
552020 <stefanolombardo@posteo.de>
2021-05-02 19:50:41 +0200

    lesson 40-1 JSX translated back in JS with JSX commented out

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -22,11 +23,20 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <Expenses items={expenses} />
-    </div>
+
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses })
   );
+
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
