a9bc40260d1d57ed7263762fc7201c7add3da84f
552020 <stefanolombardo@posteo.de>
2021-05-03 11:13:13 +0200

    lesson 46 how to add an eventlistener to a component - in the code some useful information about the different possible variations - commented out

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
   return (
     // <div className="expense-item"> // This div ha been replace through the Card component.
     <Card className="expense-item">
@@ -12,9 +16,20 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
     // </div>
   );
 }
 
 export default ExpenseItem;
+
+// TWO BUTTON VARIANTS
+
+// <button onClick={function myButtonFunction() {console.log("Clicked!");}}>Change Title</button> */
+// <button onClick={() => {console.log("Clicked!");}}>Change Title</button> */
+
+// NOTE
+
+// <button onClick={clickHandler}>Change Title</button> here is important to have {clickHandler} and not {clickHanlder()} otherwise the funtion is executed immediately withouth waiting for the click
+// You can try it and you will see that in the console appears "Clicked!!!" four times without clicking the buttons
