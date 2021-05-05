7b009a650f5374966487a9594ffef24258f6e714
552020 <stefanolombardo@posteo.de>
2021-05-05 17:48:00 +0200

    second assignment it seems to work the selected year is collected in the status of the new filter component and lifted up to the parent component

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const [selectedYear, setSelectedYear] = useState("");
+
+  const selectYearHandler = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const yearSelected = {
+    year: selectedYear,
+  };
+
+  props.onSaveSelectedYear(yearSelected);
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={selectedYear} onChange={selectYearHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
