0bcee0e11d05f268fdee1a5b0bbfbeae2dfa58a7
552020 <stefanolombardo@posteo.de>
2021-05-07 20:35:04 +0200

    assignment 4 schwarzmueller solution - I missed a button in my solution

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,7 +7,7 @@ const ExpenseForm = (props) => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  const [switchState, setSwitchState] = useState(0);
+  // const [switchState, setSwitchState] = useState(0);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -21,9 +21,9 @@ const ExpenseForm = (props) => {
     setEnteredDate(event.target.value);
   };
 
-  const onClickHandler = () => {
-    setSwitchState(1);
-  };
+  // const onClickHandler = () => {
+  //   setSwitchState(1);
+  // };
 
   const submitHandler = (event) => {
     event.preventDefault(); // to prevent auto-reload of the page
@@ -38,54 +38,58 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-    setSwitchState(0);
+    // setSwitchState(0);
   };
 
   return (
     <form onSubmit={submitHandler}>
-      {switchState === 1 ? (
-        <div>
-          <div className="new-expense__controls">
-            <div className="new-expense__control">
-              <label>Title</label>
-              <input
-                type="text"
-                value={enteredTitle}
-                onChange={titleChangeHandler}
-              />
-            </div>
-            <div className="new-expense__control">
-              <label>Amount</label>
-              <input
-                type="number"
-                min="0.01"
-                step="0.01"
-                value={enteredAmount}
-                onChange={amountChangeHandler}
-              />
-            </div>
-            <div className="new-expense__control">
-              <label>Date</label>
-              <input
-                type="date"
-                min="2019-01-01"
-                max="2022-12-31"
-                value={enteredDate}
-                onChange={dateChangeHandler}
-              />
-            </div>
+      {/* {switchState === 1 ? ( */}
+      <div>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
           </div>
-          <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
           </div>
         </div>
-      ) : (
         <div className="new-expense__actions">
-          <button type="button" onClick={onClickHandler}>
-            Add New Expense
+          <button type="button" onClick={props.onCancel}>
+            Cancel
           </button>
+          <button type="submit">Add Expense</button>
         </div>
-      )}
+      </div>
+      {/* ) : (
+      <div className="new-expense__actions">
+      <button></button>
+        <button type="button" onClick={onClickHandler}>
+          Add New Expense
+        </button>
+      </div>
+      )} */}
     </form>
   );
 };
